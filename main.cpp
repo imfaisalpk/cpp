e44ab29d0f84e0b9e709998617f2e3758ba60712
mfaisal <mfaisalpk@outlook.com>
2018-02-05 13:44:53 +0500

    Variable Done

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 // #include <stdio.h>
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     // Just the beginning
-    cout << "Hello Pakistan"<< endl;
-    cout << "and Hello World!"<< endl;
+    // cout << "Hello Pakistan"<< endl;
+    // cout << "and Hello World!"<< endl;
 
     /* I am multi-line comment*/
+
+    // VARIABLES
+
+    // int a(5), b{5}, result=0;
+    // result = a+b;
+    // cout << result << endl;
+
+    // auto a = 0;
+    // decltype(a) b;
+    // auto c = 0;
+    // a = 5;
+    // b = 4;
+    // c = a+b;
+    // cout<< c << endl;
+
+    string name = "mfaisal";
+    string company("imfaisal.com");
+    string title{"Chief Executive Officer"};
+
+    cout<< name << endl;
+    cout<< company << endl;
+    cout<< title << endl;
+
 }
