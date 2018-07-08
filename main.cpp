d27c80f1bba023a5a0925d4148fad9f13f1b342d
mfaisal <mfaisalpk@outlook.com>
2018-07-08 21:46:31 +0500

    Introduction and Variables Done

--- main.cpp
+++ main.cpp
@@ -1,36 +1,14 @@
-// #include <stdio.h>
 #include <iostream>
-#include <string>
 using namespace std;
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-    // Just the beginning
-    // cout << "Hello Pakistan"<< endl;
-    // cout << "and Hello World!"<< endl;
-
-    /* I am multi-line comment*/
-
-    // VARIABLES
-
-    // int a(5), b{5}, result=0;
-    // result = a+b;
-    // cout << result << endl;
-
-    // auto a = 0;
-    // decltype(a) b;
-    // auto c = 0;
-    // a = 5;
-    // b = 4;
-    // c = a+b;
-    // cout<< c << endl;
-
-    string name = "mfaisal";
-    string company("imfaisal.com");
-    string title{"Chief Executive Officer"};
-
-    cout<< name << endl;
-    cout<< company << endl;
-    cout<< title << endl;
-
+    // one line comment
+    /*two line 
+    comments*/
+    
+    cout << "Hello Pakistan!\n";
+    cout << "Hello again from the other side of the world!!\n";
+    
+    return 0;
 }
