72fa037ebb2f882ff3b1275ed6566f5e753beb50
mfaisal <mfaisalpk@outlook.com>
2018-07-12 00:41:25 +0500

    STRINGS DONE

--- main.cpp
+++ main.cpp
@@ -1,14 +1,158 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
 using namespace std;
+
+#define AUTHOR "MFAISAL"
+
 
 int main(int argc, char const *argv[])
 {
+
+    
+    // string str_1 = "cat";
+    // string str_2 = "dog";
+
+    // cout << str_1.replace(0,str_1.length(),"mfaisal") << endl;
+    // cout << str_1.erase(1,str_1.length()) << endl;
+    // cout << str_1.insert(str_1.find("a",0),"faisal") << endl;
+    // cout << str_1.assign(str_2,0,2) << endl;
+
+    // cout << str_1.compare(str_2) << endl;
+    // cout << str_2.compare(str_1) << endl;
+
+    // str_2.assign(str_1);
+    // cout << str_2.compare(str_1) << endl;
+
+
+    // string guess = "";
+
+    // cout<<"Provide your input: " << endl;
+    // getline(cin,guess);
+
+    // cout << "Is empty: " << guess.empty() << endl;
+    // cout << guess.append(" after append") << endl;
+    // cout << sizeof(guess) << endl;
+
+
+
+    // char name[8] = {'m','f','a','i','s','a','l','\0'};
+    // string name_str = "mfaisal";
+
+    // cout << name_str << endl;
+    // cout << name << endl;
+
+
+
+    // int num_to_be_guessed =0;
+    // string user_input="";
+
+    // do{
+    //     cout << "Guess number from1 to 10" << endl;
+    //     getline(cin,user_input);
+    //     num_to_be_guessed = stoi(user_input);
+    //     cout << num_to_be_guessed << endl;
+
+    // }while(num_to_be_guessed!=4);    
+    
+    // int rand_num = (rand()%100)+1;
+    // while(rand_num != 100){
+    //     cout << "randome number: "<<rand_num << endl;
+    //     rand_num = (rand()%100)+1;
+    // }
+
+
+    // char name[7] = {'m','f','a','i','s','a','l'};
+
+    // char _2darray[7][7] = {
+    //     {'m','f','a','i','s','a','l'},
+    //     {'m','f','a','i','s','a','l'}
+    // };
+
+    // for(int i=0;i<2;i++){
+    //     for(int j=0;j<7;j++){
+    //         cout<< _2darray[i][j] << " | ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // cout << name[0] << endl;
+
+    // _2darray[0][1]='l';
+    // cout << _2darray[0][1] << endl;
+
+
+
+
+    // int test_check_(1);
+    // string test_check="mfaisal";
+    // test_check = "muhammadfaisal";
+
+    // cout << (test_check_ == 1 ? test_check : "None") << endl;
+
+
+    // switch(test_check_){
+    //     case 1:
+    //     cout<<"css topper: " << test_check <<endl;
+    //     break;
+
+    //     case 2:
+    //     cout << "paec scientist:" << test_check << endl;
+    //     break;
+
+    //     default:
+    //     cout << "Thanks for trying!!" << endl;
+    // }
+
+
+
+
+    // if (test_check == "mfaisal"){
+    //     cout<<"css topper: " << test_check <<endl;
+    // }else if (test_check=="muhammadfaisal"){
+    //     cout << "paec scientist:" << test_check << endl;
+    // }else{
+    //     cout << "Keep work hard!!" << endl;
+    // }
+
+
+
+    // unsigned number = 5;
+    // cout << sizeof(number) << endl; 
+    // cout << "(2+1-3)*2/2 :"<< (2+1-3)*2/2 << endl;
+    // cout << (float)4/5 << endl;
+
+
+
+    // string str("");
+
+    // cout << "this is str: " << str << "\n";
+
+    // str = "mfaisal";
+
+    // cout << "again this is str: " << AUTHOR << "\n";
+
+
+    // unsigned left_digits=2, right_digits=3   ;
+    // unsigned arth_result=0;
+    // arth_result = left_digits + right_digits;
+
+    // unsigned counter(1);
+
+    // cout << "Answer: " << arth_result+counter << "\n";
+
+
     // one line comment
     /*two line 
     comments*/
     
-    cout << "Hello Pakistan!\n";
-    cout << "Hello again from the other side of the world!!\n";
+    // cout << "Hello Pakistan!\n";
+    // cout << "Hello again from the other side of the world!!\n";
+
+
+
     
     return 0;
 }
